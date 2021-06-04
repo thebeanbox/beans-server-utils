2151fd49d7715212426657b80eabacb6851d2f8b
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2021-06-04 17:16:22 -0400

    Update init.lua

--- BAU/lua/bau/init.lua
+++ BAU/lua/bau/init.lua
@@ -1,6 +1,6 @@
-file.CreateDir("bau") -- adds directory in garrysmod/data
-
-AddCSLuaFile("autorun/bau_init.lua")
+file.CreateDir("bau") -- adds directory in garrysmod/data (to be used later)
 
 include("bau/shared/commands.lua")
 AddCSLuaFile("bau/shared/commands.lua")
+
+AddCSLuaFile("autorun/bau_init.lua") -- init for client side
