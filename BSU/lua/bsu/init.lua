fbeb1e5c63d59a481faca332c8803b268cfe3e78
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2021-06-05 22:57:15 -0400

    Update init.lua

--- BSU/lua/bsu/init.lua
+++ BSU/lua/bsu/init.lua
@@ -1,4 +1,42 @@
-include("bsu/shared/commands.lua")
-AddCSLuaFile("bsu/shared/commands.lua")
+SV_DIR = "bsu/server/"
+SH_DIR = "bsu/shared/"
+CL_DIR = "bsu/client/"
 
-AddCSLuaFile("autorun/bsu_init.lua") -- init for clientside
+-- SETUP IMPORTANT FILES
+MsgN("[BSU] INITIALIZING...")
+
+include(SV_DIR .. "database.lua") -- setup the db
+include(SV_DIR .. "teams.lua") -- load team data from db
+include(SV_DIR .. "players.lua") -- assign player data from db to players and manage rank perms
+
+MsgN("[BSU] FINISHED MAIN SETUP")
+
+-- LOAD OTHER FILES
+MsgN("[BSU] LOADING FILES:")
+
+local shFiles = file.Find(SH_DIR .. "*.lua", "LUA") -- shared files
+local clFiles = file.Find(CL_DIR .. "*.lua", "LUA") -- client files
+
+local fileNum = 0
+
+for _, file in ipairs(shFiles) do -- including shared files and sending to clientside
+	fileNum = fileNum + 1
+	
+	include(SH_DIR .. file)
+	AddCSLuaFile(SH_DIR .. file)
+	
+	MsgN(" " .. fileNum .. ". shared/" .. file)
+end
+
+for _, file in ipairs(clFiles) do -- sending client files to clientside
+	fileNum = fileNum + 1
+	
+	AddCSLuaFile(CL_DIR .. file)
+	
+	MsgN(" " .. fileNum .. ". client/" .. file)
+end
+
+MsgN("[BSU] FINISHED LOADING " .. (#shFiles + #clFiles) .. " FILES")
+
+-- INIT CLIENT SIDE
+AddCSLuaFile("bsu/cl_init.lua")
