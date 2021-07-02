c4586cab1ce673b930ab69eacdf1451db55642d8
FadeRax <29849733+FadeRax@users.noreply.github.com>
2021-07-01 20:06:24 -0500

    Update statHud.lua

--- RaxScripts/lua/statHud/shared/statHud.lua
+++ RaxScripts/lua/statHud/shared/statHud.lua
@@ -7,28 +7,35 @@ if SERVER then
 else
 
     //-- Variables
-    local hideHuds = {["CHudHealth"]=true, ["CHudBattery"]=true}
+    surface.CreateFont("fontMain", {
+        font = "Arial",
+        size = 13,
+        antialias = true
+    })
+    local blur = Material("pp/blurscreen")
+    local hideHuds = {CHudHealth=true, CHudBattery=true}
     local resW,resH = ScrW(),ScrH()
-    local hudX = resW * 0.1
+    local hudX = resW * 0.05
     local hudY = resH * 0.9
     local hudW = 100
-    local hudH = 60
+    local hudH = 40
 
 
     //-- Functions
     function drawHud()
-        draw.RoundedBox(5, hudX, hudY, hudW, hudH, Color(0,0,0,50))
+        draw.RoundedBox(5, hudX, hudY, hudW, hudH, Color(0,0,0,100))
+        draw.DrawText("Ball Fondler", "fontMain", hudX+10, hudY+10, Color(255,255,255,255), 0)
     end
 
     function hideHud(name)
-        if hide[name] then
+        if hideHuds[name] then
             return false
         end
     end
 
 
     //-- Hooks
-    hook.add("HUDPaint", "statHud_drawHud", drawHud)
-    hook.add("HUDShouldDraw", "statHud_hideHud", hideHud)
+    hook.Add("HUDPaint", "statHud_drawHud", drawHud)
+    hook.Add("HUDShouldDraw", "statHud_hideHud", hideHud)
 
 end
