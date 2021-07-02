ff3b8d8f728369aa2af5d8da87ff3b54ff640625
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-01 22:03:14 -0400

    Update anti-skybox.lua

--- BSU/lua/bsu/server/anti-skybox.lua
+++ BSU/lua/bsu/server/anti-skybox.lua
@@ -6,7 +6,7 @@ if SERVER then
 
 	pos = {--Corner1, Corner2
 		gm_flatgrass = { Vector(-7735,-7796,-16128), Vector(8535,8476,-13017) },
-		gm_bigcity_night = { Vector(3060, 3060, 4400), Vector(-3060, -3060, 5800) }
+		gm_bigcity_night = { Vector(3060, 3060, 4200), Vector(-3060, -3060, 5800) }
 	} -- in the future this would probably be integrated into a gui of sorts, updated dynamically so if an admin sees that the map doesnt have the corners set, we can add them on the fly.
 
     if not pos[game.GetMap()] then print("BSU Skybox Protection - this map does not have vectors set for the skybox! this will not operate until vectors are set.") end
@@ -22,15 +22,18 @@ if SERVER then
 		local c1 = pos[game.GetMap()][1]
 		local c2 = pos[game.GetMap()][2]
 		local lents = ents.FindInBox(c1, c2)
+
+		for i, v in ipairs( player.GetAll() ) do
+	        util.AddNetworkString("BSU_SkyboxNetMessage") -- get the networking for the hud ready to go
+			net.Start("BSU_SkyboxNetMessage")
+			net.WriteBool(v:GetPos():WithinAABox(c1, c2))
+			net.Send(v)
+		end
+
 		for i=1, #lents do
 	        local current = lents[i]
 			if !current:IsPlayer() && !current:GetClass()=="predicted_viewmodel" then
 				skyboxRemove(current:GetOwner(), current)
-	        elseif current:IsPlayer() then
-				util.AddNetworkString("BSU_SkyboxNetMessage")
-				net.Start("BSU_SkyboxNetMessage")
-				net.WriteBool(true)
-				net.Send(current)
 	        end
 end
 end)
