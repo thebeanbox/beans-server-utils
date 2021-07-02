1d85aa9dbee162aa2a4e0dcb6624c9e0872c0fe7
FadeRax <29849733+FadeRax@users.noreply.github.com>
2021-07-02 14:26:51 -0500

    fuck sex

--- BSU/lua/autorun/statHud.lua
+++ BSU/lua/autorun/statHud.lua
@@ -7,13 +7,9 @@ if SERVER then
 else
 
     //-- Variables
-    surface.CreateFont("fontMain", {
-        font = "Arial",
-        size = 15,
-        antialias = true,
-        weight = 600
-    })
-    local blur = Material("pp/blurscreen")
+    surface.CreateFont("fontMain", {font = "Arial", size = 15, antialias = true, weight = 600})
+    net.Receive("BSU_SkyboxNetMessage", function() inSkybox = net.ReadBool() end)
+    local blur = Material("pp/blurscreen") //-- USE THIS!!!
     local hideHuds = {CHudHealth=true, CHudBattery=true}
     local resW,resH = ScrW(),ScrH()
     local hudX = resW * 0.025
@@ -24,26 +20,22 @@ else
     local inSkybox
     local inSkyStr = "loading..."
     local randomNumber = math.random(0, 9999)
+    local statHud = vgui.Create("DFrame")
+        statHud:SetPos(hudX, hudY)
+        statHud:SetSize(hudW, hudH)
+        statHud:SetSizable(false)
+        statHud:SetScreenLock(false)
+        statHud:ShowCloseButton(false)
+        statHud:SetDraggable(false)
+        statHud:SetTitle("")
 
-    net.Receive("BSU_SkyboxNetMessage", function()
-        inSkybox = net.ReadBool()
-    end)
 
     //-- Functions
-    function drawHud()
+    function drawHud(self, w, h)
         local plyHealth = math.Clamp(lPly:Health(), 0, 999)
         local plyArmor = math.Clamp(lPly:Armor(), 0, 999)
 
-        
-
-        draw.DrawText("v. "..randomNumber, "fontMain", hudX+5, hudY-10, Color(255,0,0,255), 0)
-
-        draw.RoundedBox(5, hudX, hudY, hudW, hudH, Color(0,0,0,100))
-        draw.DrawText("Health: "..plyHealth, "fontMain", hudX+5, hudY+5, Color(255,255,255,255), 0)
-        draw.DrawText("Armor: "..plyArmor, "fontMain", hudX+5, hudY+20, Color(255,255,255,255), 0)
-        
-        if inSkybox then inSkyStr = "In skybox" else inSkyStr = "Not in skybox" end
-        draw.DrawText(inSkyStr, "fontMain", hudX+5, hudY+35, Color(255,0,0,255), 0)
+        draw.RoundedBox(0, 0, 0, w, h, Color(255, 0, 255, 100))
     end
 
     function hideHud(name)
@@ -52,9 +44,9 @@ else
         end
     end
 
-
+    
     //-- Hooks
-    hook.Add("HUDPaint", "statHud_drawHud", drawHud)
+    statHud.Paint = drawHud
     hook.Add("HUDShouldDraw", "statHud_hideHud", hideHud)
 
 end
