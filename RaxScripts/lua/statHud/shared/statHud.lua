c39f86e5007346592396f2ce0f68a7ae11abeb86
FadeRax <29849733+FadeRax@users.noreply.github.com>
2021-07-01 21:12:03 -0500

    Update statHud.lua

--- RaxScripts/lua/statHud/shared/statHud.lua
+++ RaxScripts/lua/statHud/shared/statHud.lua
@@ -9,22 +9,40 @@ else
     //-- Variables
     surface.CreateFont("fontMain", {
         font = "Arial",
-        size = 13,
-        antialias = true
+        size = 15,
+        antialias = true,
+        weight = 600
     })
     local blur = Material("pp/blurscreen")
     local hideHuds = {CHudHealth=true, CHudBattery=true}
     local resW,resH = ScrW(),ScrH()
-    local hudX = resW * 0.05
-    local hudY = resH * 0.9
+    local hudX = resW * 0.025
+    local hudY = resH * 0.925
     local hudW = 100
     local hudH = 40
+    local lPly = LocalPlayer()
+    local inSkyStr = "Dummy Text!"
+    local randomNumber = math.random(0, 9999)
 
+    net.Receive("BSU_SkyboxNetMessage", function()
+        inSkybox = net.ReadBool()
+    end)
 
     //-- Functions
     function drawHud()
+        local plyHealth = math.Clamp(lPly:Health(), 0, 999)
+        local plyArmor = math.Clamp(lPly:Armor(), 0, 999)
+
+        
+
+        draw.DrawText("v. "..randomNumber, "fontMain", hudX+5, hudY-10, Color(255,0,0,255), 0)
+
         draw.RoundedBox(5, hudX, hudY, hudW, hudH, Color(0,0,0,100))
-        draw.DrawText("Ball Fondler", "fontMain", hudX+10, hudY+10, Color(255,255,255,255), 0)
+        draw.DrawText("Health: "..plyHealth, "fontMain", hudX+5, hudY+5, Color(255,255,255,255), 0)
+        draw.DrawText("Armor: "..plyArmor, "fontMain", hudX+5, hudY+20, Color(255,255,255,255), 0)
+        
+        if inSkybox then inSkyStr = "YOU ARE IN SKYBOX!!!" else inSKyStr = "YOU ARE NOT IN THE SKYBOX!!!!!!" end
+        draw.DrawText(inSkyStr, "fontMain", hudX+5, hudY+35, Color(255,0,0,255), 0)
     end
 
     function hideHud(name)
