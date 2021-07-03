d9cc49c2817bbe7bde15882fe79d8442fc4938df
FadeRax <29849733+FadeRax@users.noreply.github.com>
2021-07-02 19:49:28 -0500

    updated stathud

--- lua/autorun/client/statHud.lua
+++ lua/autorun/client/statHud.lua
@@ -10,12 +10,13 @@ else
     surface.CreateFont("fontMain", {font = "Arial", size = 15, antialias = true, weight = 600})
     net.Receive("BSU_SkyboxNetMessage", function() inSkybox = net.ReadBool() end)
     local blur = Material("pp/blurscreen") //-- USE THIS!!!
+    local plyHealth, plyArmor = 0, 0
     local hideHuds = {CHudHealth=true, CHudBattery=true}
     local resW,resH = ScrW(),ScrH()
     local hudX = resW * 0.025
     local hudY = resH * 0.925
-    local hudW = 100
-    local hudH = 40
+    local hudW = 150
+    local hudH = 30
     local lPly = LocalPlayer()
     local inSkybox
     local inSkyStr = "loading..."
@@ -34,16 +35,16 @@ else
     //-- Functions
     function drawHud(self, w, h)
         local barWidth = w-10
-        local plyHealth = math.Clamp((lPly:Health()/100)*barWidth, 0, barWidth)
-        local plyArmor = math.Clamp((lPly:Armor()/100)*barWidth, 0, barWidth)
+        plyHealth = Lerp(0.1, plyHealth, math.Clamp((lPly:Health()/lPly:GetMaxHealth())*barWidth, 0, barWidth))
+        plyArmor = Lerp(0.1, plyArmor, math.Clamp((lPly:Armor()/lPly:GetMaxArmor())*barWidth, 0, barWidth))
 
         -- Draw Background
-        draw.RoundedBox(5, 0, 0, w, h, Color(0, 0, 0, 100))
+        draw.RoundedBox(10, 0, 0, w, h, Color(0, 0, 0, 100))
         -- Draw Health Bar
-        draw.RoundedBox(3, 5, 5, plyHealth, 20, Color(255, 0, 0, 255))
-        draw.RoundedBox(3, 5, 5, barWidth, 20, Color(0, 255, 0, 255))
+        draw.RoundedBox(10, 5, 5, barWidth, 20, Color(200, 0, 0, 255))
+        draw.RoundedBox(10, 5, 5, plyHealth, 20, Color(0, 255, 0, 255))
         -- Draw Armor Bar
-        draw.RoundedBox(5, 5, 20, plyArmor, 5, Color(0, 200, 255, 255))
+        draw.RoundedBox(20, 5, 20, plyArmor, 5, Color(0, 150, 255, 255))
     end
 
     function hideHud(name)
