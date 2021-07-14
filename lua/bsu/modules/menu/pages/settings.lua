b5062f3789c58917b755eb1760adf5f6d3ca20d3
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 02:15:47 -0400

    Update settings.lua

--- lua/bsu/modules/menu/pages/settings.lua
+++ lua/bsu/modules/menu/pages/settings.lua
@@ -1,8 +1,14 @@
 local panel = vgui.Create("DPanel")
 panel.Paint = function() end
 
-local entry = vgui.Create("DTextEntry", panel)
-entry:SetTabbingDisabled(true) -- this is needed because you have to be holding TAB to open the menu
-entry:Dock(TOP)
+-- DEVTESTING FUNCTIONS, PLEASE FUCKING REMOVE THESE BEFORE THE SERVER GOES PUBLIC
+
+local entry1 = vgui.Create("DNumSlider", panel)
+entry1:SetTabbingDisabled(true) -- this is needed because you have to be holding TAB to open the menu
+entry1:Dock(TOP)
+entry1:SetText("Team Setter (dev function)")
+entry1:SetMinMax(100, 108)
+entry1:SetDefaultValue(LocalPlayer():Team())
+entry1:SetConVar("bsu_SetPlayerRank \"" .. LocalPlayer():Nick() .. "\"")
 
 bsuMenu.addPage(3, "Settings", panel, "icon16/cog.png")
