38be549f183dadbd41ec13b3e7f4d010a272f74d
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 13:27:59 -0400

    Update anti-skybox.lua

--- lua/bsu/server/anti-skybox.lua
+++ lua/bsu/server/anti-skybox.lua
@@ -3,56 +3,11 @@
 
 if SERVER then
 	print("BSU Skybox Protection - initalizing!")
-    dataFile = "bsu/anti-skybox_data.json"
+    local map = game.GetMap()
+    sql.Query("CREATE TABLE IF NOT EXISTS bsu_skybox_vectors(mapName TEXT PRIMARY KEY, x1 REAL, x2 REAL, y1 REAL, y2 REAL, z1 REAL, z2 REAL)")
+    sql.Query(string.format("INSERT INTO bsu_skybox_vectors(mapName, corner1, corner2) VALUES('%s', '%f', '%f', '%f', '%f', '%f', '%f')", "gm_flatgrass", -7735, -7796, -16128, 8535, 8476, -13317)
 
-    if not file.Exists("bsu/", "DATA") then
-        file.CreateDir("bsu/")
-        print("BSU Skybox Protection - data/bsu did not exist! creating directory, the fact that it did not exist is probably very bad!")
-    end
+    local mapData = sql.QueryRow(string.format("SELECT * FROM bsu_skybox_vectors WHERE mapName = '%s'"), map)
 
-    if not file.Exists(dataFile, "DATA") then
-        local baseFileWrite = {
-            gm_flatgrass = { Vector(-7735,-7796,-16128), Vector(8535,8476,-13317) },
-            gm_bigcity_night = { Vector(3060, 3060, 4200), Vector(-3060, -3060, 5800) }
-        }
-        file.Write(dataFile, util.TableToJSON(baseFileWrite))
-        print("BSU Skybox Protection - data/" .. dataFile .. " did not exist (this is bad!) creating file, make sure to add skybox data for the current map!")
-    end
-    skyboxDataTable = any
-	pos = any
-
-    function reloadSkyboxFile()
-        print("BSU Skybox Protection - reloading skybox dataset!")
-        skyboxDataTable = file.Read(dataFile, "DATA")
-        PrintTable(util.JSONToTable(skyboxDataTable))
-        pos = util.JSONToTable(skyboxDataTable)
-	util.AddNetworkString("BSU_SkyboxNetMessage") -- get the networking for the hud ready to go
-	if !pos[game.GetMap()] then print("BSU Skybox Protection - this map does not have vectors set for the skybox! this will not operate until vectors are set.") end
-    end
-    timer.Simple(3, reloadSkyboxFile)
-
-	hook.Add("Think", "BSU_SkyboxCheck", function()
-		if not pos[game.GetMap()] then
-		 -- idk
-		else
-			local c1 = pos[game.GetMap()][1]
-			local c2 = pos[game.GetMap()][2]
-			local fents = ents.FindInBox(c1, c2)
-
-			for i, v in ipairs( player.GetAll() ) do
-				net.Start("BSU_SkyboxNetMessage")
-				net.WriteBool(v:GetPos():WithinAABox(c1, c2))
-				net.Send(v)
-			end
-
-			for i=1, #fents do
-				local current = fents[i]
-				if !current:IsPlayer() and !current:GetClass()=="predicted_viewmodel" then
-					print("a!")
-					current:Remove()
-					current:GetOwner():PrintMessage(HUD_PRINTTALK, "you aren't permitted to build here yet!")
-				end
-			end
-		end
-	end)
+    PrintTable(mapData)
 end
