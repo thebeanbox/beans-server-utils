d4e20faaf67df97728bdd2580ead63d20a52ff85
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 15:40:30 -0400

    Update admin.lua

--- lua/bsu/modules/menu/pages/admin.lua
+++ lua/bsu/modules/menu/pages/admin.lua
@@ -1,6 +1,9 @@
 local panel = vgui.Create("DPanel")
 panel.Paint = function() end
 
+local entry = vgui.Create("DTextEntry", panel)
+entry:SetTabbingDisabled(true) -- this is needed because you have to be holding TAB to open the menu
+entry:Dock(TOP)
 
 --if bsu.Permissions.plyHasPermission(LocalPlayer(), "bsu_isStaffMember") then
     bsuMenu.addPage(4, "Moderation", panel, "icon16/monkey.png")
