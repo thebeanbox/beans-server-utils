3b259e8060943445ac55fc28b6111907cccea157
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 04:08:05 -0400

    Update settings.lua

--- lua/bsu/modules/menu/pages/settings.lua
+++ lua/bsu/modules/menu/pages/settings.lua
@@ -1,20 +1,8 @@
 local panel = vgui.Create("DPanel")
 panel.Paint = function() end
 
--- DEVTESTING FUNCTIONS, PLEASE FUCKING REMOVE THESE BEFORE THE SERVER GOES PUBLIC
-
-local entry1 = vgui.Create("DNumberWang", panel)
-hook.Add("InitPostEntity", "BSU_LayoutDevSettings", function()
-  entry1:Dock(TOP)
-  entry1:SetDecimals(0)
-  entry1:SetMinMax(100, 112)
-  entry1:SetPos(85, 25)
-  entry1.OnValueChanged = function(self)
-    LocalPlayer():SetTeam(self:GetValue())
-  end
-end )
-local entry2 = vgui.Create("DLabel")
-entry2:SetText("Set Team")
-entry2:SetPos(15, 25)
+local entry = vgui.Create("DTextEntry", panel)
+entry:SetTabbingDisabled(true) -- this is needed because you have to be holding TAB to open the menu
+entry:Dock(TOP)
 
 bsuMenu.addPage(3, "Settings", panel, "icon16/cog.png")
