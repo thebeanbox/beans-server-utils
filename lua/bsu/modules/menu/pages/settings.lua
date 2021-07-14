80fefda6ffa495eead603c6685bc291291421a27
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 02:36:42 -0400

    Update settings.lua

--- lua/bsu/modules/menu/pages/settings.lua
+++ lua/bsu/modules/menu/pages/settings.lua
@@ -4,10 +4,10 @@ panel.Paint = function() end
 -- DEVTESTING FUNCTIONS, PLEASE FUCKING REMOVE THESE BEFORE THE SERVER GOES PUBLIC
 
 local entry1 = vgui.Create("DNumSlider", panel)
-entry1:Dock(TOP)
+timer.Simple(2, entry1:Dock(TOP)
 entry1:SetText("Team Setter (dev function)")
 entry1:SetMinMax(100, 108)
 entry1:SetDefaultValue(LocalPlayer():Team())
-entry1:SetConVar("bsu_SetPlayerRank \"" .. LocalPlayer():Nick() .. "\"")
+entry1:SetConVar("bsu_SetPlayerRank \"" .. LocalPlayer():Nick() .. "\""))
 
 bsuMenu.addPage(3, "Settings", panel, "icon16/cog.png")
