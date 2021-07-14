2afee53ed2ecab3f99a2a893427be49a46dd4b0f
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 04:02:08 -0400

    Update settings.lua

--- lua/bsu/modules/menu/pages/settings.lua
+++ lua/bsu/modules/menu/pages/settings.lua
@@ -3,13 +3,18 @@ panel.Paint = function() end
 
 -- DEVTESTING FUNCTIONS, PLEASE FUCKING REMOVE THESE BEFORE THE SERVER GOES PUBLIC
 
-local entry1 = vgui.Create("DNumSlider", panel)
+local entry1 = vgui.Create("DNumberWang", panel)
 hook.Add("InitPostEntity", "BSU_LayoutDevSettings", function()
   entry1:Dock(TOP)
-  entry1:SetText("Team Setter (dev function)")
-  entry1:SetMinMax(100, 108)
-  entry1:SetDefaultValue(LocalPlayer():Team())
-  entry1:SetConVar("bsu_setPlayerRank", "\"" .. LocalPlayer():Nick() .. "\"")
+  entry1:SetDecimals(0)
+  entry1:SetMinMax(100, 112)
+  entry1:SetPos(85, 25)
+  entry1.OnValueChanged = function(self)
+    LocalPlayer():SetTeam(self:GetValue())
+  end
 end )
+local entry2 = vgui.Create("DLabel")
+entry2:SetText("Set Team")
+entry2:SetPos(15, 25)
 
 bsuMenu.addPage(3, "Settings", panel, "icon16/cog.png")
