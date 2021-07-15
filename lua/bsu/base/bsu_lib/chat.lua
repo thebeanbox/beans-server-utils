d3e2e9b3f9559ab115a780a1b83bf57368817214
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 22:41:03 -0400

    Update chat.lua

--- lua/bsu/base/bsu_lib/chat.lua
+++ lua/bsu/base/bsu_lib/chat.lua
@@ -1,13 +1,13 @@
 if SERVER then
 	util.AddNetworkString("bsu_sendchatmessagetoall")
-	function BSU:SendChatMessageToAll(varargs ToSend)
+	function BSU:SendChatMessageToAll(data)
 		net.Start("bsu_sendchatmessagetoall")
-		net.WriteType(ToSend)
-        net.Broadcast()
+		net.WriteType(util.Compress(util.TableToJSON(data))
+        	net.Broadcast()
     end
 elseif CLIENT then
-    net.Receive("bsu_sendchatmessagetoall", function()
-        str = net.ReadTable()
-        chat.AddText(Color(235, 179, 68), "[BSU]", Color(255, 174, 97))
+    net.Receive("bsu_sendchatmessagetoall", function(len)
+        str = util.JSONToTable(util.Decompress(net.ReadData(len)))
+        chat.AddText(Color(235, 179, 68), "[BSU]", Color(255, 174, 97), unpack(data))
     end)
 end
