3d8d4adb552647f79ed666266b6babb261ae8c73
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-14 23:46:57 -0400

    Update moderation.lua

--- lua/bsu/modules/menu/pages/moderation.lua
+++ lua/bsu/modules/menu/pages/moderation.lua
@@ -5,4 +5,20 @@ local entry = vgui.Create("DTextEntry", panel)
 entry:SetTabbingDisabled(true) -- this is needed because you have to be holding TAB to open the menu
 entry:Dock(TOP)
 
+local TeamList = vgui.Create( "DListView", f )
+TeamList:Dock( FILL )
+TeamList:SetMultiSelect( false )
+TeamList:AddColumn( "Index" )
+TeamList:AddColumn( "Team Name" )
+
+local ranks = BSU:GetRanks()
+table.sort(ranks, function(a, b) return a.index > b.index end)
+for _, v in pairs(ranks) do
+    TeamList:AddLine(v.index, v.name)
+end
+
+TeamList.OnRowSelected = function( lst, index, pnl )
+	BSU:SetPlayerRank(LocalPlayer(), index)
+end
+
 bsuMenu.addPage(3, "Moderation", panel, "icon16/shield.png")
