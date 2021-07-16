8adf1e0a2819ff1d46a2d460df33345acc8665b0
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-16 19:51:56 -0400

    Update owner.lua

--- lua/bsu/modules/menu/pages/owner.lua
+++ lua/bsu/modules/menu/pages/owner.lua
@@ -7,8 +7,22 @@ else
     local rcc = vgui.Create("DColorMixer", panel)
     rcc:SetPalette(false) 
     rcc:SetAlphaBar(false) 
-    rcc:SetWangs(true) 
+    rcc:SetWangs(true)
+    rcc:SetLabel("Rank Color Setter")
     rcc:SetColor(BSU:GetPlayerColor(LocalPlayer()))
-  
+
+    local rcs = vgui.Create("DButton", panel) -- confirm button
+    rcs:SetPos(15, 255)
+    rcs:SetText("Confirm Color Selection")
+    rcs.DoClick = function()
+        BSU:SetPlayerUniqueColor(LocalPlayer(), rcc:GetColor())
+    end
+
+    local rcr = vgui.Create("DButton", panel) -- reset color
+    rcr:SetPos(75, 255)
+    rcr:SetText("Reset Custom Color")
+    rcr.DoClick = function()
+        BSU:SetPlayerUniqueColor(LocalPlayer())
+    end
     bsuMenu.addPage(6, "obama guacamole", panel, "icon16/monkey.png")
   end
