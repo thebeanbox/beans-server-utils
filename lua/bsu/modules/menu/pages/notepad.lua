f5b093044e8f0ce25245906c719b1479699086cc
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-15 22:22:52 -0400

    i dont know how the fuck to use vgui and net lib

--- lua/bsu/modules/menu/pages/notepad.lua
+++ lua/bsu/modules/menu/pages/notepad.lua
@@ -1,14 +1,25 @@
 if SERVER then
-    
-
+  util.AddNetworkString("BSU_menuNotepadData")
+  
+  net.Receive("BSU_menuNotepadData", function(_, ply)
+    if BSU:PlayerIsStaff(ply) then
+      net.Start("BSU_menuNotepadData")
+        net.WriteData(util.Compress(file.Read("bsu/notepad.txt", "DATA"))
+      net.Send(ply)
+    end
+  end)
 else
   local panel = vgui.Create("DPanel")
   panel.Paint = function() end
-
+  
   local html = vgui.Create("DHTML", frame)
-    html:Dock(FILL)
-    local url = file.Read("bsu/notepad.txt", "DATA")
-    html:OpenURL(url)
+  html:Dock(FILL)
+  
+  net.Start("BSU_menuNotepadData")
+  net.SendToServer()
 
-  bsuMenu.addPage(5, "Notepad", panel, "icon16/pencil.png") -- add this page to the client's menu as "MOTD"
+  net.Receive("BSU_menuNotepadData", function(len)
+    local url = util.Decompress(net.ReadData(len))
+    html:OpenURL(url)
+  end)
 end
