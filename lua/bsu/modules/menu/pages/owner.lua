b064d2a937f3078f5d6e3e7acc27d04a82d2f8ce
wisp <82253244+wisp22@users.noreply.github.com>
2021-07-16 20:15:50 -0400

    Update owner.lua

--- lua/bsu/modules/menu/pages/owner.lua
+++ lua/bsu/modules/menu/pages/owner.lua
@@ -1,4 +1,14 @@
 if SERVER then
+    util.AddNetworkString("BSU_MenuChangeUniqueColor")
+    net.Receive("BSU_MenuChangeUniqueColor", function() 
+        local changereset = net.ReadBool() -- if true, change color, if false, reset color
+        local ply = net.ReadEntity()
+        if changereset == true then
+            BSU:SetPlayerUniqueColor(ply, net.ReadColor())
+        elseif changereset == false then
+            BSU:ResetPlayerUniqueColor(ply)
+        end
+    end)
 
 else
     local panel = vgui.Create("DPanel")
@@ -15,15 +25,21 @@ else
     rcs:SetPos(15, 255)
     rcs:SetText("Confirm Color Selection")
     rcs.DoClick = function()
-        Msg(LocalPlayer():IsValid())
-        BSU:SetPlayerUniqueColor(LocalPlayer(), rcc:GetColor())
+        net.Start("BSU_MenuChangeUniqueColor")
+        net.Writebool(true)
+        net.WriteEntity(LocalPlayer())
+        net.WriteColor(rcc:GetColor())
+        net.SendToServer()
     end
 
     local rcr = vgui.Create("DButton", panel) -- reset color
     rcr:SetPos(75, 255)
     rcr:SetText("Reset Custom Color")
     rcr.DoClick = function()
-        BSU:ClearPlayerUniqueColor(LocalPlayer())
+        net.Start("BSU_MenuChangeUniqueColor")
+        net.Writebool(false)
+        net.WriteEntity(LocalPlayer())
+        net.SendToServer()
     end
     bsuMenu.addPage(6, "obama guacamole", panel, "icon16/monkey.png")
   end
