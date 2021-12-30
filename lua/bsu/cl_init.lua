af317da160eafcc55b8f21ad8a516ec0def9ccd3
A_Normal_Twig <1andonlytwig@gmail.com>
2021-12-30 17:28:23 -0500

    Module loading

--- lua/bsu/cl_init.lua
+++ lua/bsu/cl_init.lua
@@ -8,3 +8,19 @@ include(clBaseDir .. "pp.lua")
 
 -- networking
 include(clBaseDir .. "networking.lua")
+
+-- Module loading
+local clModulesDir = BSU.DIR_MODULES .. "client/"
+
+local shModules = file.Find(BSU.DIR_MODULES .. "*.lua", "LUA")
+local clModules = file.Find(clModulesDir .. "*.lua", "LUA")
+
+--Run shared modules
+for _, module in ipairs(shModules) do
+  include(BSU.DIR_MODULES .. module)
+end
+
+-- Run client-side modules
+for _, module in ipairs(clModules) do
+  include(clModulesDir .. module)
+end
--- lua/bsu/sv_init.lua
+++ lua/bsu/sv_init.lua
@@ -22,7 +22,8 @@ AddCSLuaFile(clBaseDir .. "pp.lua")
 -- networking
 AddCSLuaFile(clBaseDir .. "networking.lua")
 
---[[
+
+-- Module loading
 local svModulesDir = BSU.DIR_MODULES .. "server/"
 local clModulesDir = BSU.DIR_MODULES .. "client/"
 
@@ -30,7 +31,18 @@ local shModules = file.Find(BSU.DIR_MODULES .. "*.lua", "LUA")
 local svModules = file.Find(svModulesDir .. "*.lua", "LUA")
 local clModules = file.Find(clModulesDir .. "*.lua", "LUA")
 
--- run module scripts
+-- Run server-side modules
+for _, module in ipairs(svModules) do
+  include(svModulesDir .. module)
+end
 
+-- Run shared modules
+for _, module in ipairs(shModules) do
+  include(BSU.DIR_MODULES .. module)
+  AddCSLuaFile(BSU.DIR_MODULES .. module)
+end
 
-]]
+-- Include client-side modules
+for _, module in ipairs(clModules) do
+  AddCSLuaFile(clModulesDir .. module)
+end
