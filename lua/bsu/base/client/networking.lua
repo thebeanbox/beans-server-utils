410c5fb099fd61401e67286740af3f2de42f3c13
A_Normal_Twig <1andonlytwig@gmail.com>
2021-12-30 17:14:30 -0500

    Replaced net.WriteTable in BSU.ClientRPC()

--- lua/bsu/base/client/networking.lua
+++ lua/bsu/base/client/networking.lua
@@ -2,7 +2,13 @@
 
 local function handleRPC()
   local funcStr = net.ReadString()
-  local args = net.ReadTable()
+
+  local len = net.ReadInt(16)
+  local args = {}
+  for i = 1, len do
+    local arg = net.ReadType()
+    table.insert(args, arg)
+  end
 
   local func = BSU.FindVar(funcStr)
   if not func or type(func) ~= "function" then return error("Received bad RPC, invalid function (" .. funcStr .. ")!") end
