e3dc89ac2d88cf3b5cec3756e624f9807303f9b8
Bonyoze <ravaloni.emv@gmail.com>
2021-12-30 21:12:14 -0500

    privilege check fix

--- lua/bsu/base/server/privileges.lua
+++ lua/bsu/base/server/privileges.lua
@@ -1,53 +1,48 @@
 -- base/server/privileges.lua
 
-local function notifyRestricted(ply, msg)
-  BSU.ClientRPC(ply, "notification.AddLegacy", "'" .. msg .. "' is restricted", NOTIFY_ERROR, 5)
+local function notifyRestricted(ply, name)
+  BSU.ClientRPC(ply, "notification.AddLegacy", "'" .. name .. "' is restricted", NOTIFY_ERROR, 5)
 end
 
 local function checkModelPrivilege(ply, model)
-  local allowed = BSU.IsPlayerAllowed(ply, BSU.PRIVILEGE_MODEL, model)
+  local allowed = BSU.PlayerIsAllowed(ply, BSU.PRIV_MODEL, model)
   if not allowed then
     notifyRestricted(ply, BSU.TrimModelPath(model))
     return false
   end
-  return true
 end
 
 local function checkNPCPrivilege(ply, npc)
-  local allowed = BSU.IsPlayerAllowed(ply, BSU.PRIVILEGE_NPC, npc)
+  local allowed = BSU.PlayerIsAllowed(ply, BSU.PRIV_NPC, npc)
   if not allowed then
     notifyRestricted(ply, npc)
     return false
   end
-  return true
 end
 
 local function checkSENTPrivilege(ply, ent)
-  local allowed = BSU.IsPlayerAllowed(ply, BSU.PRIVILEGE_SENT, ent)
+  local allowed = BSU.PlayerIsAllowed(ply, BSU.PRIV_SENT, ent)
   if not allowed then
     notifyRestricted(ply, ent)
     return false
   end
-  return true
 end
 
 local function checkSWEPPrivilege(ply, wep)
-  local allowed = BSU.IsPlayerAllowed(ply, BSU.PRIVILEGE_SWEP, wep)
+  local allowed = BSU.PlayerIsAllowed(ply, BSU.PRIV_SWEP, wep)
   if not allowed then
     notifyRestricted(ply, wep)
     return false
   end
-  return true
 end
 
 -- note: if the server cvar 'toolmode_allow_<tool>' is set to 0 then this doesn't get called
 local function checkToolPrivilege(ply, _, tool)
-  local allowed = BSU.IsPlayerAllowed(ply, BSU.PRIVILEGE_TOOL, tool)
+  local allowed = BSU.PlayerIsAllowed(ply, BSU.PRIV_TOOL, tool)
   if not allowed then
     notifyRestricted(ply, tool)
     return false
   end
-  return true
 end
 
 hook.Add("PlayerSpawnObject", "BSU_CheckModelPrivilege", checkModelPrivilege)
