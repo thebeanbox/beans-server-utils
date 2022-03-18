1e6d10371f314bc6446c76ec3311d8594432f5fa
Bonyoze <ravaloni.emv@gmail.com>
2022-03-17 22:09:36 -0400

    update msg commands

--- lua/bsu/lib/server/util.lua
+++ lua/bsu/lib/server/util.lua
@@ -1,11 +1,23 @@
 -- lib/server/util.lua
 
 -- send a console message to a player or list of players
-function BSU.ConMsg(plys, ...)
+-- or set target to NULL to send in the server console
+function BSU.SendConMsg(plys, ...)
+  if plys == NULL then
+    MsgC(...)
+    MsgN()
+    return
+  end
   BSU.ClientRPC(plys, "BSU.ConMsg", ...)
 end
 
 -- send a chat message to a player or list of players
-function BSU.ChatMsg(plys, ...)
+-- or set target to NULL to send in the server console
+function BSU.SendChatMsg(plys, ...)
+  if plys == NULL then
+    MsgC(...)
+    MsgN()
+    return
+  end
   BSU.ClientRPC(plys, "chat.AddText", ...)
 end
