33a40adacb52b1b856e431084a74dac3950633b7
Bonyoze <ravaloni.emv@gmail.com>
2022-03-20 18:42:01 -0400

    moved commands concommand.Add to base

--- lua/bsu/base/server/commands.lua
+++ lua/bsu/base/server/commands.lua
@@ -1,5 +1,16 @@
 -- base/server/commands.lua
 
+-- create the concommand
+concommand.Add("bsu", function(ply, _, args, argStr)
+  if not args[1] then return end
+  local name = string.lower(args[1])
+  local cmd = BSU.GetCommandByName(name)
+  if not cmd then return BSU.SendConMsg(ply, color_white, "Unknown BSU command: " .. name) end
+
+  -- execute the command
+  BSU.RunCommand(name, ply, string.sub(argStr, #name + 2))
+end, nil, nil, FCVAR_CLIENTCMD_CAN_EXECUTE)
+
 -- allow command usage in chat
 hook.Add("PlayerSay", "BSU_RunChatCommand", function(ply, text)
   if not string.StartWith(text, BSU.CMD_PREFIX) then return end
--- lua/bsu/lib/server/commands.lua
+++ lua/bsu/lib/server/commands.lua
@@ -502,13 +502,3 @@ function BSU.RunCommand(name, ply, argStr)
   end
   BSU.UnsafeRunCommand(name, ply, argStr)
 end
-
-concommand.Add("bsu", function(ply, _, args, argStr)
-  if not args[1] then return end
-  local name = string.lower(args[1])
-  local cmd = cmds[name]
-  if not cmd then return BSU.SendConMsg(ply, color_white, "Unknown BSU command: " .. name) end
-
-  -- execute the command
-  BSU.RunCommand(name, ply, string.sub(argStr, #name + 2))
-end, nil, nil, FCVAR_CLIENTCMD_CAN_EXECUTE)
