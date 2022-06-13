7a76862d53f8300ea0e125837276c11a4ecfdc5a
Bonyoze <ravaloni.emv@gmail.com>
2022-06-13 19:00:06 -0400

    update help command formatting

--- lua/bsu/modules/commands/client/misc.lua
+++ lua/bsu/modules/commands/client/misc.lua
@@ -1,3 +1,5 @@
+local desc_color = Color(180, 180, 180)
+
 --[[
   Name: help
   Desc: Show a list of all available BSU commands
@@ -5,21 +7,22 @@
 BSU.SetupCommand("help", function(cmd)
   cmd:SetDescription("Show a list of all available BSU commands")
   cmd:SetFunction(function(self, ply)
-    local str = "\n\n[BSU COMMAND LIST]\n\n"
+    local msg = { color_white, "\n\n[BSU COMMAND LIST]\n\n" }
     local categories = BSU.GetCommandCategories()
 
     for k, v in ipairs(categories) do
-      str = str .. "[" .. string.upper(v) .. "]:"
+      table.Add(msg, { color_white, "[" .. string.upper(v) .. "]:" })
+
       local cmds = BSU.GetCommandsByCategory(v)
       table.sort(cmds, function(a, b) return a:GetName() < b:GetName() end)
       
       for _, vv in ipairs(cmds) do
-        str = str .. "\n\t" .. vv:GetName() .. "\n\t- " .. vv:GetDescription()
+        table.Add(msg, { color_white, "\n\t" .. vv:GetName(), desc_color, "\n\t- " .. vv:GetDescription() })
       end
-      str = str .. "\n\n"
+      table.insert(msg, "\n\n")
     end
 
-    MsgC(color_white, str)
+    MsgC(unpack(msg))
     chat.AddText("Command help list has been printed to console")
   end)
 end)
