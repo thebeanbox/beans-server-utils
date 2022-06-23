ce0b68450e80826bedd8767d8e4d4072a5faed10
Bonyoze <ravaloni.emv@gmail.com>
2022-06-22 23:29:55 -0400

    limits now checks if you're superadmin

--- lua/bsu/base/server/limits.lua
+++ lua/bsu/base/server/limits.lua
@@ -1,6 +1,7 @@
 -- base/server/limits.lua
 
 local function checkLimit(ply, limitName, currAmt)
+  if ply:IsSuperAdmin() then return true end
   local amt = BSU.GetPlayerLimit(ply:SteamID64(), limitName)
   if amt then return amt < 0 or currAmt < amt end
 end
