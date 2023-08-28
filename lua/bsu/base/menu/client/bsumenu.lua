133a4432eff100e7c24f6e9f66323aff7b88677e
FadeRax <29849733+FadeRax@users.noreply.github.com>
2023-08-28 10:04:38 -0500

    is it working

--- lua/bsu/base/menu/client/bsumenu.lua
+++ lua/bsu/base/menu/client/bsumenu.lua
@@ -53,37 +53,3 @@ function bsuMenu:InitializeTabs()
 end
 
 vgui.Register("BSUMenu", bsuMenu, "DFrame")
-
-local function createBSUMenu()
-	if BSU.BSUMenu then BSU.BSUMenu:Remove() end
-	
-	local bsuMenu = vgui.Create("BSUMenu")
-	BSU.BSUMenu = bsuMenu
-	hook.Run("BSU_BSUMenuInit", bsuMenu)
-	bsuMenu:InitializeTabs()
-	hook.Run("BSU_BSUMenuPostInit", bsuMenu)
-end
-
-BSU.SetupCommand("menu", function(cmd)
-	cmd:SetDescription("Opens the menu")
-	cmd:SetCategory("utility")
-	cmd:SetAccess(BSU.CMD_ANYONE)
-	cmd:SetFunction(function(self, caller)
-		if not BSU.BSUMenu then createBSUMenu() end
-		BSU.BSUMenu:Center()
-		BSU.BSUMenu:Show()
-		BSU.BSUMenu:MakePopup()
-	end)
-end)
-
-BSU.SetupCommand("menuregen", function(cmd)
-	cmd:SetDescription("Regenerates the menu")
-	cmd:SetCategory("utility")
-	cmd:SetAccess(BSU.CMD_ANYONE)
-	cmd:SetFunction(function(self, caller)
-		if BSU.BSUMenu then
-			BSU.BSUMenu:Remove()
-		end
-		createBSUMenu()
-	end)
-end)
