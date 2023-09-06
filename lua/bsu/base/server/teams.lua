3f4c906625bb05745d56f4ef40fdf344d44548d2
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2023-09-06 17:38:47 -0400

    remove bot default team

--- lua/bsu/base/server/teams.lua
+++ lua/bsu/base/server/teams.lua
@@ -5,7 +5,6 @@ if next(BSU.GetAllTeams()) == nil then
 	BSU.RegisterTeam(1, "User", Color(255, 255, 255))
 	BSU.RegisterTeam(2, "Admin", Color(0, 100, 0))
 	BSU.RegisterTeam(3, "Super Admin", Color(255, 0, 0))
-	BSU.RegisterTeam(4, "Bot", Color(0, 127, 255))
 end
 
 -- setup teams serverside
