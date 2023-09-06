fbe9924657a870c4bf4bc30ae544171217ec7986
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2023-09-06 17:26:51 -0400

    privilege wildcard checking option + no longer default

--- lua/bsu/lib/server/privileges.lua
+++ lua/bsu/lib/server/privileges.lua
@@ -36,7 +36,7 @@ function BSU.GetGroupWildcardPrivileges(groupid, type)
 end
 
 -- returns bool if the group is granted the privilege (or nothing if the privilege is not registered)
-function BSU.CheckGroupPrivilege(groupid, type, value)
+function BSU.CheckGroupPrivilege(groupid, type, value, checkwildcards)
 	-- check for group privilege
 	local priv = BSU.SQLSelectByValues(BSU.SQL_GROUP_PRIVS, { groupid = groupid, type = type, value = value })[1]
 
@@ -44,12 +44,14 @@ function BSU.CheckGroupPrivilege(groupid, type, value)
 		return priv.granted ~= 0
 	else
 		-- check wildcard privileges
-		local wildcards = BSU.GetGroupWildcardPrivileges(groupid, type)
-		table.sort(wildcards, function(a, b) return #a.value > #b.value end)
+		if checkwildcards then
+			local wildcards = BSU.GetGroupWildcardPrivileges(groupid, type)
+			table.sort(wildcards, function(a, b) return #a.value > #b.value end)
 
-		for _, v in ipairs(wildcards) do
-			if string.find(value, string.Replace(v.value, "*", "(.-)")) ~= nil then
-				return v.granted ~= 0
+			for _, v in ipairs(wildcards) do
+				if string.find(value, string.Replace(v.value, "*", "(.-)")) ~= nil then
+					return v.granted ~= 0
+				end
 			end
 		end
 
@@ -62,12 +64,12 @@ function BSU.CheckGroupPrivilege(groupid, type, value)
 end
 
 -- returns bool if the player is granted the privilege (or nothing if the privilege is not registered in the player's group)
-function BSU.CheckPlayerPrivilege(steamid, type, value)
+function BSU.CheckPlayerPrivilege(steamid, type, value, checkwildcards)
 	steamid = BSU.ID64(steamid)
 
 	-- check for privilege in player's group
 	local data = BSU.SQLSelectByValues(BSU.SQL_PLAYERS, { steamid = steamid })[1]
 	if data then
-		return BSU.CheckGroupPrivilege(data.groupid, type, value)
+		return BSU.CheckGroupPrivilege(data.groupid, type, value, checkwildcards)
 	end
 end
