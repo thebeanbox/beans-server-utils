5930e797bcc34319d20de33ef4abd3dd0e9a7dd9
FadeRax <29849733+FadeRax@users.noreply.github.com>
2023-09-13 16:44:56 -0500

    almost functional bansmenu

--- lua/bsu/base/client/menu/bansmenu.lua
+++ lua/bsu/base/client/menu/bansmenu.lua
@@ -2,11 +2,12 @@ local bansmenu = {}
 
 function bansmenu:Init()
 	self:Dock(FILL)
-
+	
 	self.bansPerPage = 50 -- 255 Max
 	self.bansRemaining = 0
 	self.page = 1
-	
+	self.bans = {}
+		
 	local headerPanel = vgui.Create("DPanel", self)
 	headerPanel:Dock(TOP)
 	headerPanel:SetHeight(25)
@@ -31,6 +32,16 @@ function bansmenu:Init()
 		self:NextPage()
 	end
 	self.nextButton = nextButton
+	
+	local refreshButton = vgui.Create("DButton", headerPanel)
+	refreshButton:Dock(LEFT)
+	refreshButton:SetWidth(25)
+	refreshButton:SetText("")
+	refreshButton:SetIcon("icon16/arrow_refresh.png")
+	refreshButton.DoClick = function()
+		self:LoadPage(self.page)
+	end
+	self.refreshButton = refreshButton
 
 	local pageLabel = vgui.Create("DLabel", headerPanel)
 	pageLabel:Dock(LEFT)
@@ -53,17 +64,75 @@ function bansmenu:Init()
 	self:LoadPage(1)
 end
 
-function bansmenu:RequestBanList()
-	net.Start("bsu_request_banlist")
-	net.WriteUInt(self.page, 8)
+-- Requests a page of bans and updates page label
+function bansmenu:LoadPage(n)
+	self.page = n
+	net.Start("bsu_request_banlist2")
+	net.WriteUInt(n, 8)
 	net.WriteUInt(self.bansPerPage, 8)
 	net.SendToServer()
 end
 
-function bansmenu:LoadPage(n)
-	self:RequestBanList()
+-- Called after internal bans table 
+function bansmenu:UpdatePage()
 	self.banList:Clear()
 	self.pageLabel:SetText("Page: " .. self.page)
+	for _, ban in ipairs(self.bans) do
+		local newEntry = self:AddEntry(ban.name, ban.steamid, ban.duration, ban.reason, ban.bannedByName, ban.dateNiceTime)
+		newEntry.banDate = ban.date
+		newEntry.bannedBySteamID = ban.bannedBySteamID
+		
+		newEntry.OnRightClick = function(s)
+			local menu = DermaMenu()
+			
+			local copyName = menu:AddOption("Copy Name", function()
+				local str = s:GetColumnText(1)
+				SetClipboardText(str)
+				notification.AddLegacy("Copied Name \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copyName:SetIcon("icon16/page_copy.png")
+			
+			local copySteamID = menu:AddOption("Copy SteamID", function()
+				local str = s:GetColumnText(2)
+				SetClipboardText(str)
+				notification.AddLegacy("Copied SteamID \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copySteamID:SetIcon("icon16/page_copy.png")
+			
+			local copyReason = menu:AddOption("Copy Reason", function()
+				local str = s:GetColumnText(3)
+				SetClipboardText(str)
+				notification.AddLegacy("Copied Reason \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copyReason:SetIcon("icon16/page_copy.png")
+			
+			local copyBannedByName = menu:AddOption("Copy Admin Name", function()
+				print("test")
+				local str = s:GetColumnText(4)
+				SetClipboardText(str)
+				notification.AddLegacy("Copied Admin Name \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copyBannedByName:SetIcon("icon16/page_copy.png")
+			
+			local copyBannedBySteamID = menu:AddOption("Copy Admin SteamID", function()
+				local str = s.bannedBySteamID
+				SetClipboardText(str)
+				notification.AddLegacy("Copied Admin SteamID \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copyBannedBySteamID:SetIcon("icon16/page_copy.png")
+			
+			local copyDate = menu:AddOption("Copy Date", function()
+				local str = s.banDate
+				SetClipboardText(str)
+				notification.AddLegacy("Copied Date \"" .. str .. "\"", NOTIFY_CLEANUP, 2)
+			end)
+			copyDate:SetIcon("icon16/page_copy.png")
+			
+			menu:AddSpacer()
+			
+			menu:Open()
+		end
+	end
 end
 
 function bansmenu:PreviousPage()
@@ -78,12 +147,38 @@ function bansmenu:NextPage()
 	self:LoadPage(self.page)
 end
 
+function bansmenu:AddEntry(...)
+	return self.banList:AddLine(...)
+end
 
 vgui.Register("BSUBansMenu", bansmenu, "DPanel")
 
 hook.Add("BSU_BSUMenuInit", "BSUBansMenuInit", function(bsuMenu)
-	if not LocalPlayer():IsUserGroup("superadmin") then return end
+	if not (LocalPlayer():IsAdmin() or LocalPlayer():IsSuperAdmin()) then return end
 	bansMenu = vgui.Create("BSUBansMenu", bsuMenu)
-	bsuMenu:AddTab("Ban History", 4, bansMenu, "icon16/shield.png")
+	bsuMenu:AddTab("Ban History NEW!!!", 4, bansMenu, "icon16/shield.png")
 	BSU.BansMenu = bansMenu
+end)
+
+net.Receive("bsu_request_banlist2", function()
+	if not BSU.BansMenu then return end
+	
+	local bans = {}
+	
+	local banAmount = net.ReadUInt(8)
+	for i=1, banAmount do
+		bans[i] = {
+			name = net.ReadString(),
+			steamid = net.ReadString(),
+			reason = net.ReadString(),
+			duration = net.ReadUInt(32),
+			dateNiceTime = net.ReadString(),
+			date = net.ReadString(),
+			bannedByName = net.ReadString(),
+			bannedBySteamID = net.ReadString(),
+		}
+	end
+	
+	BSU.BansMenu.bans = bans
+	BSU.BansMenu:UpdatePage()
 end)
--- lua/bsu/base/server/menu/bansmenu.lua
+++ lua/bsu/base/server/menu/bansmenu.lua
@@ -1,26 +1,50 @@
-util.AddNetworkString("bsu_request_banlist")
+util.AddNetworkString("bsu_request_banlist2")
 
-net.Receive("bsu_request_banlist", function(len, ply)
+net.Receive("bsu_request_banlist2", function(len, ply)
+	local canSeeBans = BSU.CheckPlayerPrivilege(ply:SteamID(), BSU.PRIV_MISC, "bsu_see_bans")
+	if canSeeBans == false or canSeeBans == nil and not ply:IsAdmin() then return end
+
 	local page = net.ReadUInt(8) - 1
 	local bansPerPage = net.ReadUInt(8)
 	local allBans = BSU.GetAllBans()
-	local bansRemaining = bansPerPage
+	local bans = {}
 
-	net.Start("bsu_request_banlist")
-	for banIndex = 1, bansPerPage do
-		local pageOffset = banIndex + page * 50
-		
-		username = "my awesome name"
-		steamID = "steam:1234"
-		banDuration = 1234
-		bannedByUsername = "i banned you name"
-		banDate = "monday tomorrow"
+	-- Filter out kicks and inactive bans
+	for _, ban in ipairs(allBans) do
+		if ban.duration and (not ban.unbanTime and (ban.duration == 0 or (ban.time + ban.duration * 60) > BSU.UTCTime())) then table.insert(bans, ban) end
+	end
+	
+	-- Sort remaining bans table
+	if #bans > 0 then
+		table.sort(bans, function(a, b) return a.time > b.time end)
+	end
 
-		net.WriteString(username) -- Name
-		net.WriteString(steamID) -- SteamID
-		net.WriteUInt(banDuration, 32) -- Duration (Seconds)
-		net.WriteString(bannedByUsername) -- Banned by (Name)
-		net.WriteString(banDate) -- Ban Date (Nice time)
+	local pageOffset = page * bansPerPage
+	local bansRemaining = (#bans > bansPerPage) and (#bans - bansPerPage) or 0
+	
+	net.Start("bsu_request_banlist2")
+	net.WriteUInt(#bans, 8)
+	for i = 1, #bans do
+		local ban = bans[pageOffset + i]
+		if not ban then break end
+
+		banUsername = BSU.IsValidIP(ban.identity) and ban.identity or (BSU.GetPlayerDataBySteamID(ban.identity, "name") and BSU.GetPlayerDataBySteamID(ban.identity, "name").name or "[NO NAME]")
+		banSteamID = ban.identity
+		banReason = ban.reason and ban.reason or "[NO REASON]"
+		banDuration = ban.duration
+		banDateNiceTime = os.date("%c", tonumber(ban.time))
+		banDate = ban.time
+		bannedByUsername = ban.admin and BSU.GetPlayerDataBySteamID(ban.admin, "name").name or "[CONSOLE]"
+		bannedBySteamID = ban.admin and ban.admin or "[CONSOLE]"
+
+		net.WriteString(banUsername)
+		net.WriteString(banSteamID)
+		net.WriteString(banReason)
+		net.WriteUInt(banDuration, 32)
+		net.WriteString(banDateNiceTime)
+		net.WriteString(banDate)
+		net.WriteString(bannedByUsername)
+		net.WriteString(bannedBySteamID)
 	end
 	net.Send(ply)
 end)
