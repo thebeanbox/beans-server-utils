58193f23ace1e730cd7ea1dcbe67bbfa52191693
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2023-11-04 18:22:29 -0400

    add wildcard support for group limits

--- lua/bsu/base/server/limits.lua
+++ lua/bsu/base/server/limits.lua
@@ -2,7 +2,7 @@
 
 local function checkLimit(ply, limitName, currAmt)
 	if ply:IsSuperAdmin() then return true end
-	local amt = BSU.GetPlayerLimit(ply:SteamID64(), limitName)
+	local amt = BSU.GetPlayerLimit(ply:SteamID64(), limitName, true) -- also check wildcards
 	if amt then return amt < 0 or currAmt < amt end
 end
 
