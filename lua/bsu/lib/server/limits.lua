fe97325bfdfc8ed7c7d55329b9eb33fbe3b1b36b
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2023-11-04 18:22:29 -0400

    add wildcard support for group limits

--- lua/bsu/lib/server/limits.lua
+++ lua/bsu/lib/server/limits.lua
@@ -34,8 +34,17 @@ function BSU.GetAllGroupLimits()
 	return BSU.SQLSelectAll(BSU.SQL_GROUP_LIMITS)
 end
 
+function BSU.GetGroupWildcardLimits(groupid)
+	local query = BSU.SQLQuery("SELECT * FROM '%s' WHERE groupid = %s AND name LIKE '%s'",
+		BSU.EscOrNULL(BSU.SQL_GROUP_LIMITS, true),
+		BSU.EscOrNULL(groupid),
+		"%*%"
+	)
+	return query and BSU.SQLParse(query, BSU.SQL_GROUP_LIMITS) or {}
+end
+
 -- returns the amount the group can spawn for a specific limit (or nothing if the limit is not registered) (this excludes the cvar 'sbox_max<limit name>')
-function BSU.GetGroupLimit(groupid, name)
+function BSU.GetGroupLimit(groupid, name, checkwildcards)
 	name = string.lower(name)
 
 	local limit = BSU.SQLSelectByValues(BSU.SQL_GROUP_LIMITS, { groupid = groupid, name = name })[1]
@@ -43,21 +52,33 @@ function BSU.GetGroupLimit(groupid, name)
 	if limit then
 		return limit.amount
 	else
+		-- check wildcard limits
+		if checkwildcards then
+			local wildcards = BSU.GetGroupWildcardLimits(groupid, type)
+			table.sort(wildcards, function(a, b) return #a.name > #b.name end)
+
+			for _, v in ipairs(wildcards) do
+				if string.find(name, string.Replace(v.name, "*", "(.-)")) ~= nil then
+					return v.amount
+				end
+			end
+		end
+
 		-- check for limit in inherited group
 		local inherit = BSU.GetGroupInherit(groupid)
 		if inherit then
-			return BSU.GetGroupLimit(inherit, name)
+			return BSU.GetGroupLimit(inherit, name, checkwildcards)
 		end
 	end
 end
 
 -- returns the amount the player can spawn for a specific limit (or nothing if the limit is not registered) (this excludes the cvar 'sbox_max<limit name>')
-function BSU.GetPlayerLimit(steamid, name)
+function BSU.GetPlayerLimit(steamid, name, checkwildcards)
 	steamid = BSU.ID64(steamid)
 
 	-- check for limit in player's group
 	local data = BSU.SQLSelectByValues(BSU.SQL_PLAYERS, { steamid = steamid })[1]
 	if data then
-		return BSU.GetGroupLimit(data.groupid, name)
+		return BSU.GetGroupLimit(data.groupid, name, checkwildcards)
 	end
 end
