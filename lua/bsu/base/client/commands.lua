eac753282a43634bd14c109128118b7bf81203c0
Twig <78218557+ANormalTwig@users.noreply.github.com>
2023-11-27 13:44:23 -0500

    Better Concommand Autocomplete (#65)
    * Better console autocomplete
    
    * string_format
    
    * actually I think this looks better
    
    * don't need the function anymore
    
    * forgot to format it
    
    * probably done now

--- lua/bsu/base/client/commands.lua
+++ lua/bsu/base/client/commands.lua
@@ -1,15 +1,79 @@
 -- base/client/commands.lua
 
-local function autoComplete(_, argStr)
-	local args = string.Split(string.Trim(argStr), " ")
-	local name = table.remove(args, 1)
-	if not name then return end
+local string_format = string.format
 
+local argTypeLookup = {
+	[0] = "string",
+	[1] = "number",
+	[2] = "player",
+	[3] = "players",
+}
+
+local function autoComplete(_, argStr)
+	local name = string.match(argStr, "^ ([^%s]+)") or ""
 	local cmd = BSU.GetCommandByName(name)
 
 	if cmd then
-		-- TODO: Autocomplete/suggest args
-		return {}
+		local handler = BSU.CommandHandler(LocalPlayer(), cmd, string.sub(argStr, #name + 2), true)
+
+		-- Count number of correct args
+		local n = 0
+		for i, arg in ipairs(cmd.args) do
+			if arg.kind == 0 then
+				if not handler:GetStringArg(i) then break end
+				n = n + 1
+			elseif arg.kind == 1 then
+				if not handler:GetNumberArg(i) then break end
+				n = n + 1
+			elseif arg.kind == 2 or arg.kind == 3 then
+				if not handler:GetPlayerArg(i) then break end
+				n = n + 1
+			elseif arg.kind == 3 then
+				if not handler:GetPlayersArg(i) then break end
+				n = n + 1
+			end
+		end
+		if n >= #cmd.args then return end
+
+		-- formatting magic
+		local template = string_format("bsu %s %s%%s", name, n > 0 and table.concat(handler.args, " ", 1, n) .. " " or "")
+
+		-- Arg list
+		local argTypes = {}
+		for i = n + 1, #cmd.args do
+			local arg = cmd.args[i]
+			table.insert(argTypes, string_format("<%s: %s>", arg.name, argTypeLookup[arg.kind]))
+		end
+		local argFiller = string_format(template, table.concat(argTypes, " "))
+
+		-- Custom autocomplete table.
+		-- Probably add support for a function later.
+		local autocomplete = cmd.autocomplete
+		if autocomplete then
+			local suggestions = { argFiller }
+			for _, v in ipairs(autocomplete) do
+				table.insert(suggestions, string_format(template, v))
+			end
+
+			return suggestions
+		end
+
+		-- Player suggestions
+		local arg = cmd.args[n + 1]
+		if arg.kind == 2 or arg.kind == 3 then
+			local suggestions = { argFiller }
+
+			local plyName = string.lower(handler.args[n + 1] or "")
+			for _, v in ipairs(player.GetAll()) do
+				if plyName == string.sub(string.lower(v:Nick()), 1, #plyName) then
+					table.insert(suggestions, string_format(template, string_format("\"%s\"", v:Nick())))
+				end
+			end
+
+			return suggestions
+		end
+
+		return { argFiller }
 	else
 		local result, names = {}, {}
 
