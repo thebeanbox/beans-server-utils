129e15828538b8295be45eb820e6a11523b8a285
Twig <78218557+ANormalTwig@users.noreply.github.com>
2024-01-18 22:36:49 -0500

    replace nw2vars (#74)

--- lua/bsu/base/server/pdata.lua
+++
@@ -1,9 +0,0 @@
--- base/server/pdata.lua
-
-local function networkPData(ply)
-	for k, v in pairs(BSU.GetAllPData(ply), true) do -- get only networked data
-		ply:SetNW2String("bsu_" .. k, v)
-	end
-end
-
-hook.Add("BSU_ClientReady", "BSU_NetworkPData", networkPData)
--- lua/bsu/lib/client/pdata.lua
+++ lua/bsu/lib/client/pdata.lua
@@ -1,13 +1,41 @@
 -- lib/client/pdata.lua
 
+local playerData = {}
+
+net.Receive("bsu_pdata", function()
+	local ind = net.ReadUInt(8)
+
+	local data = playerData[ind]
+	if not data then
+		data = {}
+		playerData[ind] = data
+	end
+
+	local key = net.ReadString()
+
+	if net.ReadBool() then
+		data[key] = net.ReadString()
+	else
+		data[key] = nil
+	end
+end)
+
+hook.Add("EntityRemoved", "BSU_CleanPData", function(ent, fullUpdate)
+	if fullUpdate then return end
+	local ind = ent:EntIndex()
+	if playerData[ind] then
+		playerData[ind] = nil
+	end
+end)
+
 -- gets a pdata value on a player (or default if it's not set)
 function BSU.GetPData(ply, key, default)
-	local val = (ply or LocalPlayer()):GetNW2String("bsu_" .. key, false)
-	if val then return val end
-	return default
+	local data = playerData[ply:EntIndex()]
+	return data and data[key] or default -- Lua ternary magic
 end
 
 -- gets a pdata value on a player and attempts to convert it to a number (or default if it fails to convert)
 function BSU.GetPDataNumber(ply, key, default)
 	return tonumber(BSU.GetPData(ply, key, default)) or default
 end
+
