c7489b471de5dc801ec343dbc8b848a8ca5dc743
Bonyoze <bonyoze.emv@gmail.com>
2024-03-15 21:24:50 -0400

    small convar changes

--- lua/bsu/base/server/convars.lua
+++ lua/bsu/base/server/convars.lua
@@ -1,6 +1,6 @@
 -- base/server/convars.lua
 
-CreateConVar("bsu_default_group", "user", FCVAR_ARCHIVE, "The group that new players should be assigned to")
-CreateConVar("bsu_bot_group", "user", FCVAR_ARCHIVE, "The group that bots should be assigned to")
-CreateConVar("bsu_cleanup_time", 600, FCVAR_ARCHIVE, "The time (in seconds) before disconnected players' props get cleaned up")
-CreateConVar("bsu_allow_family_sharing", 0, FCVAR_ARCHIVE, "Should Steam Family Sharing accounts be allowed to join the server")
+CreateConVar("bsu_default_group", "user", nil, "The group that new players should be assigned to")
+CreateConVar("bsu_bot_group", "user", nil, "The group that bots should be assigned to")
+CreateConVar("bsu_cleanup_time", 600, nil, "The time (in seconds) before disconnected players' props get cleaned up")
+CreateConVar("bsu_allow_family_sharing", 0, nil, "Should Steam Family Sharing accounts be allowed to join the server")
