e447956085fdbdd06a9acf337fb2b846c229ab00
Bonyoze <bonyoze.emv@gmail.com>
2025-08-10 00:34:23 -0400

    delay setting entity serverside tostring

--- lua/bsu/base/server/pp.lua
+++ lua/bsu/base/server/pp.lua
@@ -155,9 +155,11 @@ end
 -- try set the owner of newly created entities
 hook.Add("OnEntityCreated", "BSU_SetInternalOwner", function(ent)
 	if not ent:IsValid() then return end
-	ent:SetNW2String("BSU_ServerToString", tostring(ent))
 	-- need to wait a tick for the entity to initialize and other hooks/detours to have a chance to set owner
-	timer.Simple(0, function() SetInternalOwner(ent) end)
+	timer.Simple(0, function()
+		ent:SetNW2String("BSU_ServerToString", tostring(ent))
+		SetInternalOwner(ent)
+	end)
 end)
 
 -- detour some functions to catch players spawning entities and properly set entity owner
